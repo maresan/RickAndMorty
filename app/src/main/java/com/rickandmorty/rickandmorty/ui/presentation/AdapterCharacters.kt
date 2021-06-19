953f8f6f302b170c0dd5f92e6137c4daaad7c851
Miguel Arenas <maresan91@gmail.com>
2021-06-19 18:19:12 -0400

    [R&M/001] Add: UI to Character's Information. Add: English language

--- app/src/main/java/com/rickandmorty/rickandmorty/ui/presentation/AdapterCharacters.kt
+++ app/src/main/java/com/rickandmorty/rickandmorty/ui/presentation/AdapterCharacters.kt
@@ -5,9 +5,12 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.rickandmorty.rickandmorty.R
 import com.rickandmorty.rickandmorty.databinding.AdapterCharactersBinding
 import com.rickandmorty.rickandmorty.model.Character
 import com.squareup.picasso.Picasso
+
+private const val CHARACTER_INFO = "characterInformation"
 
 class AdapterCharacters(private val characters: List<Character>) :
     RecyclerView.Adapter<CharactersViewHolder>() {
@@ -19,8 +22,8 @@ class AdapterCharacters(private val characters: List<Character>) :
 
     override fun onBindViewHolder(holder: CharactersViewHolder, position: Int) {
         holder.name.text = characters[position].name
-        holder.specie.text = characters[position].species
-        holder.gender.text = characters[position].gender
+        holder.species.text = holder.itemView.resources.getString(R.string.species, characters[position].species)
+        holder.gender.text = holder.itemView.resources.getString(R.string.gender, characters[position].gender)
         Picasso.get().load(characters[position].image).into(holder.photo)
 
         holder.itemView.setOnClickListener {
@@ -34,7 +37,7 @@ class AdapterCharacters(private val characters: List<Character>) :
 
     private fun goToCharacterInformation(context: Context, character: Character) {
         val intent = Intent(context, CharacterInformation::class.java).apply {
-            putExtra("characterInformation", character)
+            putExtra(CHARACTER_INFO, character)
         }
         context.startActivity(intent)
     }
@@ -44,6 +47,6 @@ class CharactersViewHolder(binding: AdapterCharactersBinding) :
     RecyclerView.ViewHolder(binding.root) {
     val name = binding.name
     val photo = binding.photo
-    val specie = binding.specie
+    val species = binding.species
     val gender = binding.gender
 }
--- app/src/main/java/com/rickandmorty/rickandmorty/ui/presentation/CharacterInformation.kt
+++ app/src/main/java/com/rickandmorty/rickandmorty/ui/presentation/CharacterInformation.kt
@@ -1,9 +1,14 @@
 package com.rickandmorty.rickandmorty.ui.presentation
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.rickandmorty.rickandmorty.R
 import com.rickandmorty.rickandmorty.databinding.ActivityCharacterInformationBinding
 import com.rickandmorty.rickandmorty.model.Character
+import com.squareup.picasso.Picasso
+
+private const val CHARACTER_INFO = "characterInformation"
 
 class CharacterInformation : AppCompatActivity() {
     private lateinit var binding: ActivityCharacterInformationBinding
@@ -23,8 +28,9 @@ class CharacterInformation : AppCompatActivity() {
         binding = ActivityCharacterInformationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        characterInformation = intent.extras?.get("characterInformation") as Character
+        characterInformation = intent.extras?.get(CHARACTER_INFO) as Character
         initActionBar(characterInformation.name)
+        initView(characterInformation)
     }
 
     private fun initActionBar(name: String) {
@@ -33,6 +39,40 @@ class CharacterInformation : AppCompatActivity() {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.title = name
             actionBar.elevation = 0.0f
+        }
+    }
+
+    private fun initView(characterInformation: Character) {
+        Picasso.get().load(characterInformation.image).into(binding.image)
+
+        if (characterInformation.status.isNotEmpty()) {
+            binding.status.text = getString(R.string.status, characterInformation.status)
+            binding.status.visibility = View.VISIBLE
+        }
+
+        if (characterInformation.species.isNotEmpty()) {
+            binding.species.text = getString(R.string.species, characterInformation.species)
+            binding.species.visibility = View.VISIBLE
+        }
+
+        if (characterInformation.type.isNotEmpty()) {
+            binding.type.text = getString(R.string.type, characterInformation.type)
+            binding.type.visibility = View.VISIBLE
+        }
+
+        if (characterInformation.gender.isNotEmpty()) {
+            binding.gender.text = getString(R.string.gender, characterInformation.gender)
+            binding.gender.visibility = View.VISIBLE
+        }
+
+        if (characterInformation.origin.name.isNotEmpty()) {
+            binding.origin.text = getString(R.string.origin, characterInformation.origin.name)
+            binding.origin.visibility = View.VISIBLE
+        }
+
+        if (characterInformation.location.name.isNotEmpty()) {
+            binding.location.text = getString(R.string.location, characterInformation.location.name)
+            binding.location.visibility = View.VISIBLE
         }
     }
 }
