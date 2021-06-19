b0152e4a9629fb855a332310987ada2993e1f292
Miguel Arenas <maresan91@gmail.com>
2021-06-19 13:36:21 -0400

    [R&M] add: dependencies

--- app/src/main/java/com/rickandmorty/rickandmorty/ui/view/MainActivity.kt
+++ app/src/main/java/com/rickandmorty/rickandmorty/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rickandmorty.rickandmorty
+package com.rickandmorty.rickandmorty.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rickandmorty.rickandmorty.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
